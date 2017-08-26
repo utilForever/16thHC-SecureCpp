2fd04ae33d4dfa1d4293ae8c9d10d5c4fd2051c7
Chris Ohk <utilforever@gmail.com>
2017-08-26 22:06:25 +0900

    Add another example

--- ERR55-CPP/main.cpp
+++ ERR55-CPP/main.cpp
@@ -1,4 +1,6 @@
 #include <exception>
+#include <iostream>
+#include <vector>
 
 class Exception1 : public std::exception {};
 class Exception2 : public std::exception {};
@@ -13,9 +15,25 @@ void bar() throw (Exception1)
 	foo();
 }
 
+void f(std::vector<int>& v, size_t s) noexcept(true)
+{
+	v.resize(s);
+}
+
 int main()
 {
 	bar();
+
+	std::vector<int> v;
+
+	try
+	{
+		f(v, 1'000'000'000);
+	}
+	catch (std::exception& e)
+	{
+		std::cout << "Exception is occurred!" << std::endl;
+	}
 
 	return 0;
 }
