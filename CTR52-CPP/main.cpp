7385d018c6f6ee7c45f0ab46e7f20c17ccec7738
Chris Ohk <utilforever@gmail.com>
2017-08-26 11:14:24 +0900

    Add CTR52-CPP example code

---
+++ CTR52-CPP/main.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+void f(const std::vector<int> &src)
+{
+	std::vector<int> dest;
+	std::copy(src.begin(), src.end(), dest.begin());
+}
+
+int main()
+{
+	std::vector<int> v = { 10, 20, 30, 40, 50 };
+
+	f(v);
+}
