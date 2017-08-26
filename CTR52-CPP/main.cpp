e0138a30f972477792d4760cb5804f3b9d1c536f
Chris Ohk <utilforever@gmail.com>
2017-08-26 11:26:50 +0900

    Add another example

--- CTR52-CPP/main.cpp
+++ CTR52-CPP/main.cpp
@@ -6,11 +6,19 @@ void f(const std::vector<int>& src)
 	std::copy(src.begin(), src.end(), dest.begin());
 }
 
+void g()
+{
+	std::vector<int> v;
+	std::fill_n(v.begin(), 10, 0x42);
+}
+
 int main()
 {
 	std::vector<int> v = { 10, 20, 30, 40, 50 };
 
 	f(v);
+
+	g();
 	
 	return 0;
 }
